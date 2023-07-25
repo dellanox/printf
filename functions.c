f77eda1e9621a630e311a09a79f3e43cad406370
Ozioma EMEREM <code.emerem@gmail.com>
2023-07-25 14:51:42 +0100

    functions.c

--- functions.c
+++ functions.c
@@ -13,21 +13,21 @@
 int print_char(va_list types, char buffer[],
 	       int flags, int width, int precision, int size)
 {
-	\char c = va_arg(types, int);
+	char c = va_arg(types, int);
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
 /**
-    * print_string - Prints a string of characters
-    * @types: List a of arguments
-    * @buffer: Buffer array to handle print
-    * @flags:  Calculates active flags
-    * @width: get width.
-    * @precision: Precision specification
-    * @size: Size specifier
-    * Return: Number of chars printed
-    */
+ * print_string - Prints a string of characters
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 
 int print_string(va_list types, char buffer[],
 		 int flags, int width, int precision, int size)
